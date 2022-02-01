91682bb9f77e10d04007f8681036e06122004a9b
Jan Stoots <31186222+jaenster@users.noreply.github.com>
2022-02-01 19:53:29 +0100

    Support undefined values

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 export function Weaked(): PropertyDecorator {
 
     return function (target, propertyKey) {
-
-        let sym = Symbol('WeakRef-'+(propertyKey.toString()));
+        let sym = Symbol('WeakRef-' + (propertyKey.toString()));
 
         Object.defineProperty(target, propertyKey, {
             get(): object {
                 return this[sym]?.deref();
             },
             set(v: object) {
-                this[sym] = new WeakRef<object>(v);
+                this[sym] = typeof v === 'object' && v ? new WeakRef<object>(v) : undefined;
             },
         })
     }
